a0a98b1474eb9970d5c4befc8a907ec512754aa5
guy.meltzer <guymeltzer28@gmail.com>
2022-11-17 00:33:25 +0200

    fix

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,4 +1,4 @@
-/bin/bash
+#!/bin/bash
 #add fix to exercise3 here
 sudo sed -i 's/Require all denied/Require all granted/g' /etc/apache2/sites-enabled/000-default.conf
 sudo /etc/init.d/apache2 restart
