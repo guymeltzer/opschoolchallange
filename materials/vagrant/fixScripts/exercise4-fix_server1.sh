9132431d00f31e1b298679876b195c28fae2c2ad
guy.meltzer <guymeltzer28@gmail.com>
2022-11-18 10:38:15 +0200

    exercise4

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
+sudo sed -i '$ d' /etc/hosts
+sudo sed -i '$a 192.168.60.10 server1' /etc/hosts
+sudo sed -i '$a 192.168.60.11 server2' /etc/hosts
+sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
+sudo systemctl restart ssh
+cat ~/.ssh/authorized_keys >> ~/.ssh/id_rsa.pub
+ssh server2 cat ~/.ssh/authorized_keys >> ~/.ssh/authorized_keys
+echo "yes"
